81320ae9b92413adc917ee59ec4e277de317e8af
I-Dvorianska <78318812+I-Dvorianska@users.noreply.github.com>
2021-07-02 16:10:51 +0000

    Deploying to gh-pages from @ I-Dvorianska/milk-project@faf0b6ac34d0cfb0a5f6e9a8420ac0b724d41bef 🚀

--- src.42ec3730.js
+++ src.42ec3730.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/milk-project/src.7dd392fe.js.map
+//# sourceMappingURL=/milk-project/src.42ec3730.js.map
